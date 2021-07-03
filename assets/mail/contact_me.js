101bb7b88e62a17a58e30f8b04b968b304dac87f
ntselet <ntselet@hotmail.com>
2021-07-03 22:30:48 +0200

    https://dwj3642neg.execute-api.eu-west-1.amazonaws.com/default/PredictFunc

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{https://3gnc0o5v90.execute-api.eu-west-1.amazonaws.com/Dev/PredictFunc}",
+                url: "{https://vrxhkbsfdc.execute-api.eu-west-1.amazonaws.com/contactme}",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
