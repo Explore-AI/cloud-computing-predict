32edd1e98ab0ea45ece1c87bc63e8f981748d5ea
ntselet <ntselet@hotmail.com>
2021-07-05 18:27:01 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -26,7 +26,7 @@ $(function () {
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
                 url: "{https://98005irfla.execute-api.eu-west-1.amazonaws.com/rootfunction}",
                 // ------------------------------------------------------  
-                type: "PUT",
+                type: "POST",
                 data: JSON.stringify(json),
                 cache: false,
                 success: function () {
