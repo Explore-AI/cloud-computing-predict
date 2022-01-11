5b4a234712717b78fd76ff6295c34cc15a787e46
niahaarz <57945600+niahaarz@users.noreply.github.com>
2022-01-11 11:10:06 +0200

    API hopefully all

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://6u3tpmthie.execute-api.eu-west-1.amazonaws.com",
+                url: "https://ugjzj02zv5.execute-api.eu-west-1.amazonaws.com/default/ContactFormStorage",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
