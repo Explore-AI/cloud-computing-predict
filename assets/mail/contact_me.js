2a7da7cb5c16df5fdcea26bb733cc4a7fa078e2d
niahaarz <57945600+niahaarz@users.noreply.github.com>
2022-01-12 15:07:30 +0200

    Quick Test

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://ugjzj02zv5.execute-api.eu-west-1.amazonaws.com/default/ContactFormStorage",
+                url: "https://ugjzj02zv5.execute-api.eu-west-1.amazonaws.com/Predict",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
