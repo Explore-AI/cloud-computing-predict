62cf449c829ceb3cf010ec77a5f9cb0c875a21fb
thatoMatima <107154034+thatoMatima@users.noreply.github.com>
2022-10-27 15:07:35 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://pg0tj8mu75.execute-api.eu-west-1.amazonaws.com/default/portfolio_website_function",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
