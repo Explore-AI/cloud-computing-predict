1f744226e5ad08ae1520daf947dfc98dead701d5
Ashmiyu Musa <asimiyumusa@gmail.com>
2023-09-03 19:39:04 +0100

    API endpoint update

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://tlu27bsgx5.execute-api.eu-west-1.amazonaws.com/default/cloud-computing-lambda",
+                url: "https://r5w7a5rf29.execute-api.eu-west-1.amazonaws.com/cloud-computing-lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
